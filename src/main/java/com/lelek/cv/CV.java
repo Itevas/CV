b3c3ca8cbd4318020c597246fe25e0b48e581390
Vasyl Lelek <itevas@outlook.com>
2019-03-06 12:15:06 +0200

    home_list

--- src/main/java/com/lelek/cv/CV.java
+++ src/main/java/com/lelek/cv/CV.java
@@ -1,9 +1,11 @@
 package com.lelek.cv;
 
+import java.util.List;
+
 public class CV {
     private Person person = new Person();
     private Contact contact = new Contact();
-    private JobPlace jobPlace = new JobPlace();
+    private List<JobPlace> jobPlaces;
 
     public static void main(String [] args){
 
